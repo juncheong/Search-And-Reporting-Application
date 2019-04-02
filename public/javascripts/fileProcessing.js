577e7da2915812036d644420b70fda41f4452b75
Daniel Rand <daniel.rand@leapfrogbuilders.com>
2019-04-02 19:04:17 -0400

    Added search bar and jquery json processing

--- public/javascripts/fileProcessing.js
+++ public/javascripts/fileProcessing.js
@@ -7,13 +7,29 @@ function processFile() {
     	    var reader = new FileReader();
    	     	reader.onload = function(e)
         	{
-            	var mydata = JSON.parse(e.target.result);
-				alert(mydata[0].title);
-				alert(mydata[0].url);
-				alert(mydata[0].description);
+              var obj = jQuery.parseJSON(e.target.result);
+              $("#btn").after(`<br><label for = "search_Field">Search:</label>
+                              <input type="text" id="search_Field" />
+                              <input type="submit" id="btn2" onclick = "search()" />`);
         	};
         	reader.readAsBinaryString(file.files[0]);
     	}
 	}
 }
+              //console.log(obj.Result[0].title);
+function search () {
 
+}
+/*
+function processFile () {
+  var file = document.getElementById("up");
+  console.log(file);
+  var fr = new FileReader();
+  fr.onload = function(e) { 
+  	console.log(e);
+    var result = JSON.parse(e.target.result);
+    var formatted = JSON.stringify(result, null, 2);
+	alert(result);
+  }
+}
+*/
