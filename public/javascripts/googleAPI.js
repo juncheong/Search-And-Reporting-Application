c7636bd8b7e976ea0c26bd438bb21734ddd6d040
rlhirmes103 <>
2019-04-03 00:49:30 -0400

    beginnings for api

---
+++ public/javascripts/googleAPI.js
@@ -0,0 +1,11 @@
+
+ (function Search() {
+    var cx = '006407600163843634466:7n12fra2wyc';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
