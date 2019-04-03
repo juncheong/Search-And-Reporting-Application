554670f6e61cf6428514858019b19d78afc274ae
rlhirmes103 <>
2019-04-03 11:54:16 -0400

    view

--- public/javascripts/googleAPI.js
+++ public/javascripts/googleAPI.js
@@ -6,6 +6,28 @@
     gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
+//     <table class="table col-9"> 
+//     <thead class="thead-light">
+//     <tr>
+//       <th scope="col">Title</th>
+//       <th scope="col">Website URL</th>
+//       <th scope="col">Description</th>
+//     </tr>
+//   </thead>
+// <tbody>
+// List<Result> results = result.getItems();
+// for (Result r : results) {
+//  <tr>
+//   <td>r.getName()</td>
+//   <td>r.getLink()</td>
+//   <td></td>
+// </tr>
+// }
+
+//         </tbody>
+//   </table>
+
     s.parentNode.insertBefore(gcse, s);
   })();
+
 
