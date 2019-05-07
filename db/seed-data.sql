1382117d75293308727fe17f7f7ff5466950da19
Jun <jcheong@tailoredindustry.com>
2019-05-07 08:57:29 -0400

    fixed seed-data.sql to match correct db schema

--- db/seed-data.sql
+++ db/seed-data.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `sara` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `sara`;
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: sara
 -- ------------------------------------------------------
--- Server version	8.0.15
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,16 +15,21 @@ USE `sara`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `page`
+--
+
 DROP TABLE IF EXISTS `page`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `page` (
   `pageId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `lastModified` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `lastIndexed` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `timeToIndex` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`pageId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,22 +40,21 @@ CREATE TABLE `page` (
 
 LOCK TABLES `page` WRITE;
 /*!40000 ALTER TABLE `page` DISABLE KEYS */;
-INSERT INTO `page` VALUES (1,'https://www.pizzahut.com/','Order pizza online for fast delivery or carryout from a store near you. View our full menu, see nutritional information, find store locations, and more.','Pizza Hut: Pizza Delivery | Pizza Carryout | Coupons | Wings & More',NULL,NULL);
 /*!40000 ALTER TABLE `page` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `pageWord`
+-- Table structure for table `page_word`
 --
 
-DROP TABLE IF EXISTS `pageWord`;
+DROP TABLE IF EXISTS `page_word`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `pageWord` (
+CREATE TABLE `page_word` (
   `pageWordId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `wordId` int(10) unsigned DEFAULT NULL,
-  `pageId` int(10) unsigned DEFAULT NULL,
-  `wordCount` int(11) DEFAULT NULL,
+  `pageId` int(10) unsigned NOT NULL,
+  `wordId` int(10) unsigned NOT NULL,
+  `freq` int(10) unsigned NOT NULL,
   PRIMARY KEY (`pageWordId`),
   KEY `pageword_wordid_foreign` (`wordId`),
   KEY `pageword_pageid_foreign` (`pageId`),
@@ -62,12 +64,38 @@ CREATE TABLE `pageWord` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `pageWord`
+-- Dumping data for table `page_word`
 --
 
-LOCK TABLES `pageWord` WRITE;
-/*!40000 ALTER TABLE `pageWord` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pageWord` ENABLE KEYS */;
+LOCK TABLES `page_word` WRITE;
+/*!40000 ALTER TABLE `page_word` DISABLE KEYS */;
+/*!40000 ALTER TABLE `page_word` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `search`
+--
+
+DROP TABLE IF EXISTS `search`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `search` (
+  `searchId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `terms` varchar(255) NOT NULL,
+  `count` int(10) unsigned NOT NULL,
+  `searchDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `timeToSearch` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`searchId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `search`
+--
+
+LOCK TABLES `search` WRITE;
+/*!40000 ALTER TABLE `search` DISABLE KEYS */;
+/*!40000 ALTER TABLE `search` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -90,7 +118,6 @@ CREATE TABLE `word` (
 
 LOCK TABLES `word` WRITE;
 /*!40000 ALTER TABLE `word` DISABLE KEYS */;
-INSERT INTO `word` VALUES (1,'pizza');
 /*!40000 ALTER TABLE `word` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-28 22:36:30
+-- Dump completed on 2019-05-07  8:52:26
