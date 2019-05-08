0dac1a6b261ab5991915bf4bcb0f0d7e0c8ff339
Jun <jcheong@tailoredindustry.com>
2019-05-07 21:05:42 -0400

    finished and tested GET pageWord, GET search, POST search

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,21 +1,12 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
 const db = require('../util/db_' + process.env.NODE_ENV);
 
 const Word = require('../models/word');
 const Page = require('../models/page');
 const Search = require('../models/search');
 const pageWord = require('../models/pageWord');
-
-exports.getPage = (req, res, next) => {
-    res.status(200).json({
-        //JSON data to pass back here
-    });
-};
-
-exports.getWord = (req, res, next) => {
-    res.status(200).json({
-        //JSON data to pass back here
-    });
-};
 
 exports.getPageWord = (req, res, next) => {
     // /pageWord/:searchWord/:partialMatch/:caseInensitive
@@ -25,36 +16,43 @@ exports.getPageWord = (req, res, next) => {
     const caseInsensitive = req.params.caseInsensitive;
 
     let query = 
-    "SELECT * FROM `page`, `word`, `page_word` " + 
-    "WHERE `page.pageId` = `page_word.pageId` " +
-    "AND `word.wordId` = `page_word.wordId` ";
+    "SELECT * FROM page, word, page_word " + 
+    "WHERE page.pageId = page_word.pageId " +
+    "AND word.wordId = page_word.wordId ";
 
     if (partialMatch && caseInsensitive) {
-        query += "AND UPPER(`word.wordName`) LIKE UPPER(`%" + searchWord + "%`) ";
+        query += "AND UPPER(word.wordName) LIKE UPPER('%" + searchWord + "%') ";
     }
     else if (caseInsensitive) {
-        query += "AND UPPER(`word.wordName`) = UPPER(" + searchWord + ") ";
+        query += "AND UPPER(word.wordName) = UPPER(" + searchWord + ") ";
     }
     else if (partialMatch) {
-        query += "AND `word.wordName` LIKE `%" + searchWord + "%` ";
+        query += "AND word.wordName LIKE '%" + searchWord + "%' ";
     }
     else {
-        query += "AND `word.wordName` = " + searchWord + " ";
+        query += "AND word.wordName = " + searchWord + " ";
     }
 
-    query += "ORDER BY `freq`";
+    query += "ORDER BY freq";
 
-    db.execute(query);
+    const queryResult = db.execute(query);
 
     res.status(200).json({
-        //JSON data to pass back here
+        result: queryResult, 
+        queryData: [{
+            searchWord: searchWord,
+            partialMatch: partialMatch,
+            caseInsensitive: caseInsensitive
+        }]
     });
 };
 
-exports.getSearch = (req, res, next) => {
-    res.status(200).json({
-        //JSON data to pass back here
-    });
+exports.getAllSearch = (req, res, next) => {
+    Search.fetchAll()
+    .then((results) => {
+        res.send(results[0]);
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postSearch = (req, res, next) => {
@@ -65,8 +63,6 @@ exports.postSearch = (req, res, next) => {
 
     const search = new Search(null, terms, count, searchDate, timeToSearch);
     search.save();
-    const insertedSearchId = Search.getId();
-    search.id = insertedSearchId;
 
     res.status(201).json({
         message: 'Search inserted successfully',
@@ -76,6 +72,16 @@ exports.postSearch = (req, res, next) => {
 
 exports.postIndexing = (req, res, next) => {
     const url = req.body.url;
-
     
+    request(url, function (err, res, body) {
+        if(err)
+        {
+            console.log(err, "error occured while hitting URL");
+        }
+        else
+        {
+            console.log(body);
+            console.log("URL: " + url);
+        }
+    });
 }
--- routes/api.js
+++ routes/api.js
@@ -4,22 +4,16 @@ const apiController = require('../controllers/apiController');
 
 const router = express.Router();
 
-// GET /api/page
-router.get('/page', apiController.getPage);
-
-// GET /api/word
-router.get('/word', apiController.getWord);
-
 // GET /api/pageWord
 router.get('/pageWord/:searchWord/:partialMatch/:caseInsensitive', apiController.getPageWord);
 
-// GET /api/search
-router.get('/search', apiController.getSearch);
+// GET /api/search currently gets all searches
+router.get('/search', apiController.getAllSearch);
 
 // POST /api/search
 router.post('/search', apiController.postSearch);
 
-// POST /api/indexing
+// POST /api/indexing (crawling)
 router.post('/indexing', apiController.postIndexing);
 
 module.exports = router;
