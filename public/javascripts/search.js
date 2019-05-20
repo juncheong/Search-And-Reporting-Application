fdebdaa30770d09887b8211d14ba4af5505fd633
Daniel Rand <daniel.rand@leapfrogbuilders.com>
2019-05-20 00:21:55 -0400

    added search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -11,3 +11,23 @@ function postToDatabase() {
 	console.log(Http.responseText)
 	}
 }
+function search () {
+	const Http = new XMLHttpRequest();
+	var searchBar = document.getElementById('searchBar');
+	var checkBox1 = document.getElementById('Insensitive');
+	var checkBox2 = document.getElementById('Partial');
+	const word= searchBar.value;
+	var insensitive = 'false', partial = 'false';
+	if (checkBox1.checked == true)
+		insensitive = 'true';
+	if (checkBox2.checked == true)
+		partial = 'true';
+	console.log(word);
+	Http.open("GET", "/api/pageWord/"+word+"/"+partial+"/"+insensitive);	
+	//Http.setRequestHeader("Content-Type", "application/json");
+	Http.send();
+	Http.onreadystatechange=(e)=>{
+	console.log(Http.responseText)
+	//var jsonString = Http.responseText;
+	}
+}
