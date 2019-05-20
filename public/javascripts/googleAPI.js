d4203a1759d1fdb0bd8f36722075e1555567f873
rlhirmes103 <>
2019-05-20 02:23:13 -0400

    google search parsing

--- public/javascripts/googleAPI.js
+++ public/javascripts/googleAPI.js
@@ -17,9 +17,52 @@
      const paramUrl = 'https://www.googleapis.com/customsearch/v1?q=' + searchBar + '&cx=' + cx + '&key=AIzaSyBQgAEFzXn12Ke34TZuTcvhF24PW1P6XxY'; 
     console.log(paramUrl)
     Http.open("GET", paramUrl);
-    Http.send(null);
-    Http.onreadystatechange=(e)=>{
-      if (Http.readyState == 4 && Http.status == 200)
+    Http.onreadystatechange = function () {
+      if (Http.readyState === 4 && Http.status == 200){
      console.log(Http.responseText)
-} 
+     text = Http.responseText;
+     showResults();
+      }      
+    };
+    Http.send();
+  }
+
+
+  function showResults () {
+	var obj = JSON.parse(text);
+	$("table").append(`<thead class="thead-light">
+            	  <tr>
+                  <th scope="col">URL</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Description</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+  			</tbody>`);
+	var numResults = 0;
+	for (var i=0 ; i < obj.items.length ; i++) {
+		numResults++;	
+		$("tbody").append(`
+                      <tr>
+                      <td><a href="`+ obj.items[i].link + `"">` + obj.items[i].link + `</a></td>
+                      <td>`+ obj.items[i].title + `</td>
+                      <td>` + obj.items[i].snippet + `</td>
+                      </tr>
+      `);
+
+	}
+	if (numResults == 0) {
+    $("tbody").append(`
+                    <tr>
+                    <td>No Results found for query: ` + query.toLowerCase() + `</td>
+                    </tr>
+    `);
+  	}
+  	else {
+   		$("tbody").append(`
+                    <tr>
+                    <td>` + numResults + ` matches for "` + query.toLowerCase() + `"</td>
+                    </tr>
+    	`);
+  	}
   }
